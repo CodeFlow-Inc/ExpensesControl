629af262815cfac78ed1cd41e66730b1ee045405
Rafael de Oliveira Gomes <rafinha.gomo.11.og@gmail.com>
2025-01-21 19:07:35 -0300

    Nova entidade com enum tipo

---
+++ ExpensesControl/Domain/Entities/AggregateRoot/Income.cs
@@ -0,0 +1,40 @@
+﻿using Destructurama.Attributed;
+using ExpensesControl.Domain.Entities.Base;
+using ExpensesControl.Domain.Enums;
+
+namespace ExpensesControl.Domain.Entities.AggregateRoot
+{
+    /// <summary>
+    /// Represents an Income entry.
+    /// </summary>
+    public class Income : BaseEntity<int>
+    {
+        /// <summary>
+        /// User code associated with the expense.
+        /// </summary>
+        [LogMasked]
+        public int UserCode { get; set; }
+
+        /// <summary>
+        /// Description of the Income.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Amount of the Income
+        /// </summary>
+        public decimal  Amount { get; set; }
+
+        /// <summary>
+        /// Date of the Income
+        /// </summary>
+        public DateOnly ReceiptDate { get; set; }
+
+        /// <summary>
+        /// Type of the Income
+        /// </summary>
+        public TypeIncome Type {  get; set; } 
+
+
+    }
+}
---
+++ ExpensesControl/Domain/Enums/TypeIncome.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace ExpensesControl.Domain.Enums
+{
+    /// <summary>
+    /// Represents the type of income.
+    /// </summary>
+    public enum TypeIncome
+    {
+        /// <summary>
+        /// Salary.
+        /// </summary>
+        [Description("Salário")]
+        Salary = 10,
+
+        /// <summary>
+        /// Food allowance (Vale Refeição).
+        /// </summary>
+        [Description("Vale Refeição")]
+        FoodAllowance = 20,
+
+        /// <summary>
+        /// Grocery allowance (Vale Alimentação).
+        /// </summary>
+        [Description("Vale Alimentação")]
+        GroceryAllowance = 30,
+
+        /// <summary>
+        /// Bonus.
+        /// </summary>
+        [Description("Bônus")]
+        Bonus = 40,
+
+        /// <summary>
+        /// Other types of income.
+        /// </summary>
+        [Description("Outros")]
+        Others = 50
+    }
+}
