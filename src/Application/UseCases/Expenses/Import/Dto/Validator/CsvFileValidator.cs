f9b7f6018932285fc306e4fa7d8f17015e444bd7
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2025-01-26 15:13:38 -0300

    FEAT: Add CsvFileValidator and enhance ImportExpensesUseCase
    Introduce CsvFileValidator to validate CSV files in ImportExpensesUseCase.
    - Add CsvFileValidator class to check file type and headers.
    - Modify ImportExpensesUseCase to use DI for IUnitOfWork and ILogger.
    - Create CsvFileValidator instance with ExpenseRecordMap.
    - Update Handle method to include CSV validation steps.
    - Use CsvHelper library for CSV file reading and validation.

---
+++ src/Application/UseCases/Expenses/Import/Dto/Validator/CsvFileValidator.cs
@@ -0,0 +1,50 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+
+namespace ExpensesControl.Application.UseCases.Expenses.Import.Dto.Validator;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="classMap"></param>
+public class CsvFileValidator(ClassMap classMap)
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="fileName"></param>
+	/// <returns></returns>
+	public static bool IsCsvFile(string fileName)
+	{
+		return fileName.EndsWith(".csv", StringComparison.OrdinalIgnoreCase);
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="fileStream"></param>
+	/// <param name="errors"></param>
+	/// <returns></returns>
+	public bool HasValidHeaders(Stream fileStream, CsvConfiguration csvConfiguration, out List<string> errors)
+	{
+		errors = [];
+
+		using var reader = new StreamReader(fileStream);
+		using var csv = new CsvReader(reader, csvConfiguration);
+		csv.Context.RegisterClassMap(classMap);
+		csv.Read();
+		csv.ReadHeader();
+		var headerRecord = csv.HeaderRecord;
+
+		var expectedHeaders = classMap.MemberMaps.Select(m => m.Data.Names.FirstOrDefault()).ToArray();
+		if (!expectedHeaders.All(header => headerRecord!.Contains(header)))
+		{
+			errors.Add("The CSV file headers are incorrect.");
+			return false;
+		}
+
+		return true;
+	}
+}
+
--- src/Application/UseCases/Expenses/Import/ImportExpensesUseCase.cs
+++ src/Application/UseCases/Expenses/Import/ImportExpensesUseCase.cs
@@ -5,6 +5,7 @@ using ExpensesControl.Application.UseCases.Base.Response;
 using ExpensesControl.Application.UseCases.Expenses.Import.Dto.Map;
 using ExpensesControl.Application.UseCases.Expenses.Import.Dto.Request;
 using ExpensesControl.Application.UseCases.Expenses.Import.Dto.Response;
+using ExpensesControl.Application.UseCases.Expenses.Import.Dto.Validator;
 using ExpensesControl.Infrastructure.SqlServer.Repositories.Interface;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -17,25 +18,54 @@ namespace ExpensesControl.Application.UseCases.Expenses.Import;
 /// </summary>
 /// <param name="unitOfWork"></param>
 /// <param name="logger"></param>
-public class ImportExpensesUseCase(IUnitOfWork unitOfWork, ILogger<ImportExpensesUseCase> logger) : IRequestHandler<ImportExpensesRequest, ImportExpensesResponse>
+public class ImportExpensesUseCase(
+	IUnitOfWork unitOfWork,
+	ILogger<ImportExpensesUseCase> logger) : IRequestHandler<ImportExpensesRequest, ImportExpensesResponse>
 {
+	/// <summary>
+	/// 
+	/// </summary>
+	private readonly CsvFileValidator csvFileValidator = new(new ExpenseRecordMap());
+
 	/// <summary>
 	/// Handles the process of importing expenses from a CSV file.
 	/// </summary>
 	/// <param name="request"></param>
 	/// <param name="cancellationToken"></param>
 	/// <returns></returns>
-	public async Task<ImportExpensesResponse> Handle(ImportExpensesRequest request, CancellationToken cancellationToken)
+	public async Task<ImportExpensesResponse> Handle(
+		ImportExpensesRequest request,
+		CancellationToken cancellationToken)
 	{
 		var response = new ImportExpensesResponse();
 		var result = new ImportResponse();
 
-		using (var stream = new StreamReader(request.File.OpenReadStream()))
-		using (var csv = new CsvReader(stream, new CsvConfiguration(CultureInfo.InvariantCulture)))
+		if (!CsvFileValidator.IsCsvFile(request.File.FileName))
 		{
+			return response.AddErrorMessage<ImportExpensesResponse>("The file is not a CSV.");
+		}
+
+		using (var stream = new StreamReader(request.File.OpenReadStream()))
+		{
+			var configuration = new CsvConfiguration(CultureInfo.CurrentCulture)
+			{
+				Delimiter = ";",
+				HasHeaderRecord = true,
+				IgnoreBlankLines = true,
+				TrimOptions = TrimOptions.Trim,
+			};
+
+			if (!csvFileValidator.HasValidHeaders(stream.BaseStream, configuration, out var headerErrors))
+			{
+				return response.AddErrorMessages<ImportExpensesResponse>(headerErrors);
+			}
+
+			using var csv = new CsvReader(stream, configuration);
 			csv.Context.RegisterClassMap<ExpenseRecordMap>();
 			var records = csv.GetRecords<ExpenseRecord>().ToList();
+
 			await unitOfWork.BeginTransactionAsync(cancellationToken);
+
 			foreach (var record in records)
 			{
 				try
